1f8be4be11aea66f678b81d2562c8e0dd88bda51
cetobi <andre.marcdias@gmail.com>
2021-08-21 21:16:55 -0300

    adicionando ex03 do matrizes

--- PLA_exercicios/AULA03 - matrizes/ex03.py
+++ PLA_exercicios/AULA03 - matrizes/ex03.py
@@ -1,21 +1,38 @@
-from typing import Generic
-
+#a+b-4*c
 
 a = [[0,3],[2,-5]]
 b = [[-2,4],[0,-1]]
 c = [[4,2],[-6,0]]
 
-def matriz(a,b,c):
-    d = []
-    lin = len(a)
-    col = len(a[0]) 
-    for i in range(lin):
-        d.append([])
-        for j in range(col):
-            e = a[i][j]+b[i][j]
-            f = -4*c[i][j]
-            g = e[i][j]*f[i][j]
-            d.append(g)
-    return d
+def exibir(a):
+    for linha in a:
+        print(linha)
 
-print(matriz(a,b,c))
+linA = len(a)
+colA = len(a[0])
+
+d = []
+for i in range(linA):
+    linha = [0]*colA
+    d.append(linha)      
+    for j in range(colA):
+        d[i][j] = a[i][j] + b[i][j]
+
+
+e = []
+for i in range(linA):
+    linha = [0]*colA
+    e.append(linha)
+    for j in range(colA):
+        e[i][j] = -4*c[i][j]
+
+
+f = []
+for i in range(linA):
+    linha = [0]*colA
+    f.append(linha)
+    for j in range(colA):
+        f[i][j] = d[i][j]*e[i][j]
+
+
+exibir(f)
